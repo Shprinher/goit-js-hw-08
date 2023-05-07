e905a23412130a890f78e33e2b9e4899b2b6bf4e
Shprinher <119606175+Shprinher@users.noreply.github.com>
2023-05-07 18:37:23 +0300

    add first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
+// // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// // Change code below this line
 
-console.log("galleryItems");
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const createGalleryItem = ({ preview, original, description }) => {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+  `;
+};
+const renderGallery = (items) => {
+  const galleryMarkup = items.map(createGalleryItem).join('');
+  galleryContainer.innerHTML = galleryMarkup;
+};
+
+renderGallery(galleryItems);
+
+const lightbox = new SimpleLightbox(".gallery__link", { captionsData: "alt", captionDelay: "250" });
