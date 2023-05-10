511e0c31f3ba0297cbb13dc38d689855af690e8c
Shprinher <119606175+Shprinher@users.noreply.github.com>
2023-05-10 09:55:56 +0300

    done eight HW

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+const currentTimeKey = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle((event) => {
+  const currentTime = Math.round(event.seconds);
+  localStorage.setItem(currentTimeKey, currentTime);
+}, 1000));
+
+const currentTime = localStorage.getItem(currentTimeKey);
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const feedbackFormStateKey = 'feedback-form-state';
+
+// Функція для запису стану форми в локальне сховище
+const saveFormState = throttle(() => {
+  const formState = { email: emailInput.value, message: messageInput.value };
+  localStorage.setItem(feedbackFormStateKey, JSON.stringify(formState));
+}, 500);
+
+// Функція для відновлення стану форми з локального сховища
+const restoreFormState = () => {
+  const formState = JSON.parse(localStorage.getItem(feedbackFormStateKey));
+  if (formState) {
+    emailInput.value = formState.email || '';
+    messageInput.value = formState.message || '';
+  }
+};
+
+// Заповнюємо поля форми при завантаженні сторінки
+window.addEventListener('load', restoreFormState);
+
+// Записуємо стан форми в локальне сховище при вводі користувача
+emailInput.addEventListener('input', saveFormState);
+messageInput.addEventListener('input', saveFormState);
+
+// Очищуємо сховище та поля форми при сабміті
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formState = { email: '', message: '' };
+  localStorage.setItem(feedbackFormStateKey, JSON.stringify(formState));
+  emailInput.value = '';
+  messageInput.value = '';
+  console.log(formState);
+});
